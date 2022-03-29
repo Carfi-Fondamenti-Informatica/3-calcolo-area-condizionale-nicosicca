c1cde5204dde8b412658bfa30090eadd9f1dc895
nicosicca <100024661+nicosicca@users.noreply.github.com>
2022-03-29 11:34:22 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main() {
+   
+   double a,b;
+   int opz;
+   
+   cin>>a>>b>>opz;
+   
+   switch(opz)
+   {
+       case 0: cout<<a*b*0.5<<endl; break;
+       case 1: cout<<a*a<<endl; break;
+       case 2: cout<<a*b<<endl; break;
+       default: cout<<"opzione non valida"<<endl; break;
+   }
    return 0;
 }
